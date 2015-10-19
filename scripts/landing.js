76b011607fe914f829be5cd80b31b6ce575b0063
Christopher Robin <millsapchristopherrobin@gmail.com>
2015-10-19 12:14:48 -0500

    updated landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,43 @@
-            var animatePoints = function () {
-            
-                var points = document.getElementsByClassName("point");
-                
-                var revealPoint = function (index) {
-                     points[index].style.opacity = 1;
-                     points[index].style.transform = "scaleX(1) translate(0)";
-                     points[index].style.msTransform = "scaleX(1) translateY(0)";
-                     points[index].style.WebkitTransform = "scaleX(1) translate(0)";
-                }
-                
-                for (i = 0; i < points.length; i++){
-                 revealPoint(i);   
-                }
+           
+var pointsArray = document.getElementsByClassName("point");
 
-            };
+var animatePoints = function (points) {
                 
-            //animatePoints();
-   
+var revealPoint = function (index) {
+     points[index].style.opacity = 1;
+     points[index].style.transform = "scaleX(1) translate(0)";
+     points[index].style.msTransform = "scaleX(1) translateY(0)";
+     points[index].style.WebkitTransform = "scaleX(1) translate(0)";
+};
+                
+    for (i = 0; i < points.length; i++){
+     revealPoint(i);   
+    }
+
+};
+
+//animatePoints();
+
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function (event) {
+
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+
+};
+
+
+ 
